581df6164fcda68cae34b4836d2f498b829782d7
Dan Lubbers <lubbers.dan@gmail.com>
2019-01-19 13:45:11 -0500

    two kata8s

--- Kata-8/kyu8-filter-out-the-geese.js
+++ Kata-8/kyu8-filter-out-the-geese.js
@@ -38,4 +38,4 @@ function gooseFilter(birds) {
 
 console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]),["Mallard", "Hook Bill", "Crested", "Blue Swedish"]);
 console.log(gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]),["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]);
-console.log(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]),[]);
+// console.log(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]),[]);
