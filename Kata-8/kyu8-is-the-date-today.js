fd15377d527469eae1d3611c136c2bc0896553bd
Dan Lubbers <lubbers.dan@gmail.com>
2019-01-22 15:35:35 -0500

    kata8

---
+++ Kata-8/kyu8-is-the-date-today.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/is-the-date-today
+
+// Write a simple function that takes as a parameter a date object and returns a boolean value representing whether the date is today or not.
+
+// Make sure that your function does not return a false positive by just checking just the day.
+
+// My Answer
+function isToday(date) {
+    return date.toDateString() === new Date().toDateString();
+}
+
+var tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+var yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+
+console.log(isToday(new Date()), true);
+console.log(isToday(tomorrow), false);
+console.log(isToday(yesterday), false);
