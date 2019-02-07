84d636465b84da9683e158161b02245a769d28a9
Dan Lubbers <lubbers.dan@gmail.com>
2019-02-07 12:35:51 -0500

    kata7

---
+++ Kata-7/kyu7-js-golfing-1-undefined.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/js-golfing-number-1-undefined/train/javascript
+
+// Return undefined with no more than 10 characters.
+
+// Digits and letters are not allowed!
+// Do not modify x! (no inputs are allowed)
+
+// My Answer ' An empty Object will return "Undefined" '
+x=()=>{}
+
+console.log(x(), undefined, "Should return undefined!");
