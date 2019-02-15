6291661a5f8e4f277eb5ec93a91c586f41a1a439
Dan Lubbers <lubbers.dan@gmail.com>
2019-02-15 13:49:51 -0500

    kata8

---
+++ Kata-8/kyu8-make-uppercase.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/makeuppercase
+
+// Write function makeUpperCase.
+
+// My Answer
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+console.log(makeUpperCase("hello"), "HELLO");
