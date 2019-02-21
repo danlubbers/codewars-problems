a861493ebf63ba093675bb1b95953f4af3c2d289
Dan Lubbers <lubbers.dan@gmail.com>
2019-02-21 11:12:15 -0500

    kata8

---
+++ Kata-8/kyu8-!a==a?!.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/a-equals-equals-a
+
+// You task to pass only this tests :
+
+// a == false
+// !a == false
+// a == !a
+
+// My Answer
+const a = [];
+
+// Other Answers
+// const a = '0';
+// const a = new Boolean()
+// const a = ' ';
+
+
+console.log( a == false,  true, `a != false`);
+console.log( !a == false, true, `!a != false`);
+console.log( a == !a,     true, `a != !a`);
