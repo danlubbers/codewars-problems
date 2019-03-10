f7840891b8dde3e619e3488f106327de385d3515
Dan Lubbers <lubbers.dan@gmail.com>
2019-03-09 19:00:45 -0500

    kata8

---
+++ Kata-8/kyu8-regexp-basics-is-it-a-digit.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/regexp-basics-is-it-a-digit
+
+// Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+
+// My Answer and Best Practice
+String.prototype.digit = function() {
+    // ^ matches the start of a string
+    // d matches any digit character
+    // $ matches the end of the string
+    // g is a global search, doesn't return after the first match
+    return /^\d$/g.test(this)
+};
+
+console.log(''.digit(), false);
+console.log('7'.digit(), true);
+console.log(' '.digit(), false);
+console.log('a'.digit(), false);
+console.log('a5'.digit(), false);
+console.log('14'.digit(), false);
