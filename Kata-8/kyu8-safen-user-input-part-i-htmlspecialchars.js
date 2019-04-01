b3640ba7b7a50dd3e111fc9ffc8bd9f5c2dc8bef
Dan Lubbers <lubbers.dan@gmail.com>
2019-04-01 13:57:49 -0400

    kata8

---
+++ Kata-8/kyu8-safen-user-input-part-i-htmlspecialchars.js
@@ -0,0 +1,48 @@
+// https://www.codewars.com/kata/safen-user-input-part-i-htmlspecialchars/train/javascript
+
+// Safen User Input Part I - htmlspecialchars
+// You are a(n) novice/average/experienced/professional/world-famous Web Developer (choose one) who owns a(n) simple/clean/slick/beautiful/complicated/professional/business website (choose one or more) which contains form fields so visitors can send emails or leave a comment on your website with ease. However, with ease comes danger. Every now and then, a hacker visits your website and attempts to compromise it through the use of XSS (Cross Site Scripting). This is done by injecting script tags into the website through form fields which may contain malicious code (e.g. a redirection to a malicious website that steals personal information).
+
+// Mission
+// Your mission is to implement a function htmlspecialchars() that converts the following potentially harmful characters:
+
+// < --> &lt;
+// > --> &gt;
+// " --> &quot;
+// & --> &amp;
+// Good luck :D
+
+// Extension
+// If you are an experienced Javascript programmer, try shortening your code as much as possible and optimise it to minimise run time. Experienced programmers should be able to complete this exercise in one line of code.
+
+// My Answer
+function htmlspecialchars(formData) {
+    let regex = /[<>"&]/g;
+    return formData.replace(regex,
+    char => ({ '<': '&lt;', '>': '&gt;', '"': '&quot;', '&': '&amp;' }[char]));
+}
+
+// Best Practice and my frist attempt 
+function htmlspecialchars(formData) {
+    return formData.replace(/&/g, '&amp;')
+                   .replace(/"/g, '&quot;')
+                   .replace(/</g, '&lt;')
+                   .replace(/>/g, '&gt;')
+}
+
+// Using Object as variable
+function htmlspecialchars(formData) {
+    const dict = {
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        '&': '&amp;',
+    };
+    return formData.replace(/[<>"&]/g, char => dict[char]);
+}
+
+
+console.log(htmlspecialchars('Hello World'), '&lt;h2&gt;Hello World&lt;/h2&gt;')
+console.log(htmlspecialchars('Hello, how would you & I fare?'), 'Hello, how would you &amp; I fare?')
+console.log(htmlspecialchars('How was "The Matrix"?  Did you like it?'), 'How was &quot;The Matrix&quot;?  Did you like it?')
+
