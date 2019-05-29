fb442c8ea26452b1ec5a039fbfc1b2b37c3e589b
Dan Lubbers <lubbers.dan@gmail.com>
2019-05-29 10:55:40 -0400

    kata8

---
+++ Kata-8/kyu8-training-js-number-11-loop-statement-break-continue.js
@@ -0,0 +1,31 @@
+// https://www.codewars.com/kata/training-js-number-11-loop-statement-break-continue/train/javascript
+
+// Coding in function ```grabDoll```. function accept 1 parameter:```dolls```. it's a string array, a list of some dolls.
+
+// You need traverse ```dolls``` by using ```for``` loop. If element is  "Hello Kitty" or "Barbie doll", you should push it to a ```bag```(bag is an array, I've defined in the function); if it's other strings, we should use ```continue``` skip it. 
+
+// When the ```bag``` has three element, ```bag``` is full. You should use ```break``` jump out the loop; If ```bag``` is not full, you should traverse ```dolls``` until the last element. 
+
+// Return the ```bag``` after for loop finished.
+
+// You should use ```for```, ```break``` and ```continue``` in your code. otherwise, your solution may not pass this kata.
+
+// My Answer & Best Practice
+function grabDoll(dolls){
+    var bag=[];
+    for(let i = 0; i < dolls.length; i++) {
+        if(dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
+            bag.push(dolls[i]) 
+        } else continue;
+        if (bag.length >= 3) break;
+    }
+    return bag;
+}
+
+console.log(grabDoll(["Mickey Mouse","Hello Kitty","Snow white"]),["Hello Kitty"]);
+
+console.log(grabDoll(["Mickey Mouse","Hello Kitty","Hello Kitty","Snow white"]),["Hello Kitty","Hello Kitty"]);
+
+console.log(grabDoll(["Mickey Mouse","Hello Kitty","Hello Kitty","Barbie doll","Snow white"]),["Hello Kitty","Hello Kitty","Barbie doll"]);
+
+console.log(grabDoll(["Mickey Mouse","Barbie doll","Hello Kitty","Hello Kitty","Hello Kitty","Snow white"]),["Barbie doll","Hello Kitty","Hello Kitty"]);
