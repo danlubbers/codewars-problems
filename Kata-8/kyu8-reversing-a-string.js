4ad3fa2853a2bc9d887d25c6a1dee604d10be3d9
Dan Lubbers <lubbers.dan@gmail.com>
2019-06-17 11:15:38 -0400

    kata8

---
+++ Kata-8/kyu8-reversing-a-string.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/reversing-a-string/train/javascript
+
+// Create a function that will reverse a string.
+
+// You might have to convert the string into an array first before you are able to reverse it.
+
+// Your final answer must also be a string.
+
+// Some functions you might find useful to solve this problem are: String.split() Array.reverse() Array.join()
+
+function reverseString(str) {
+    return str.split('').reverse('').join('');
+}
+
+console.log(reverseString("test"), "tset");
+console.log(reverseString("nice"), "ecin");
