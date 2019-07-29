548631f577610a067df5b813e8d5faa0f869bbf6
Dan Lubbers <lubbers.dan@gmail.com>
2019-07-29 11:16:39 -0400

    kata7

---
+++ Kata-7/kyu7-brl-currency-format.js
@@ -0,0 +1,35 @@
+// https://www.codewars.com/kata/brl-currency-format/train/javascript
+
+// Format a number as BRL money, i.e. R$ 10,00
+
+// More examples
+
+// currencyBRL(10) => 'R$ 10,00'
+// currencyBRL(10.5) => 'R$ 10,50'
+// currencyBRL(10.05) => 'R$ 10,05'
+// currencyBRL(.5) => 'R$ 0,50'
+// The values must be rounded to the nearest value, like:
+
+// currencyBRL(10.505) => 'R$ 10,51'
+// currencyBRL(10.504) => 'R$ 10,50'
+// currencyBRL(10.502) => 'R$ 10,50'
+// currencyBRL(10.509) => 'R$ 10,51'
+
+// My Answer
+function currencyBRL(number) {
+    if (number === undefined) {
+        return `R$ 0,00`;
+    } else {
+        return `R$ ${number.toFixed(2).replace('.', ',')}`;
+    }
+}
+
+// Best Practice
+function currencyBRL(number = 0) {
+    return 'R$ ' + number.toFixed(2).replace('.', ',');
+}
+
+console.log(currencyBRL(), 'R$ 0,00');
+console.log(currencyBRL(10), 'R$ 10,00');
+console.log(currencyBRL(10.505), 'R$ 10,51');
+console.log(currencyBRL(10.509), 'R$ 10,51');
