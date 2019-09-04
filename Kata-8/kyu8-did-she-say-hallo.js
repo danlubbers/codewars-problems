0a45ac0e42af27cc9d4f33f04ebb94cb6b9d45d3
Dan Lubbers <lubbers.dan@gmail.com>
2019-09-04 17:05:29 -0400

    kata8

---
+++ Kata-8/kyu8-did-she-say-hallo.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/did-she-say-hallo/train/javascript
+
+// You received a whatsup message from an unknown number. Could it be from that girl/boy with a foreign accent you met yesterday evening?
+
+// Write a simple regex to check if the string contains the word hallo in different languages.
+
+// These are the languages of the possible people you met the night before:
+
+// hello - english
+// ciao - italian
+// salut - french
+// hallo - german
+// hola - spanish
+// ahoj - czech republic
+// czesc - polish
+// By the way, how cool is the czech republic hallo!!
+
+// PS. you can assume the input is a string. PPS. to keep this a beginner exercise you don't need to check if the greeting is a subset of word ('Hallowen' can pass the test)
+
+// PS. regex should be case insensitive to pass the tests
+
+function validateHello(greetings) {
+    return /(hello|ciao|salut|hallo|hola|ahoj|czesc)/i.test(greetings); 
+   
+}
+
+console.log(validateHello('ahoj'), true);
+
+console.log(validateHello('meh'), false);
