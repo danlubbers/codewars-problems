f62d57644102bde82f539ab2a1476f382a74907b
Dan Lubbers <lubbers.dan@gmail.com>
2019-09-25 17:07:04 -0400

    kata8

---
+++ Kata-8/kyu8-grasshopper-function-syntax-debugging.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/grasshopper-function-syntax-debugging/train/javascript
+
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+// My Answer & Best Practice
+function main(verb, noun) {
+  return verb + noun
+};
+
+
+console.log(main('take ', 'item'), 'take item')
+console.log(main('use ', 'sword'), 'use sword')
