dd4dbd2b8f18c3241353788dba314cf59dc81249
Dan Lubbers <lubbers.dan@gmail.com>
2019-09-30 17:00:43 -0400

    kata7

---
+++ Kata-7/kyu7-sort-arrays-1.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/sort-arrays-1/train/javascript
+
+// Just a simple sorting usage. Create a function that returns the elements of the input-array in a sorted manner.
+
+// My Answer
+function sortme(names) {
+    return names.sort();
+}
+
+console.log(sortme(['one', 'two', 'three']))
