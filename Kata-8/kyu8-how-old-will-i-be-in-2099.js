fc885557b950024d2fa9fc24a45a9f4ed11ab6c8
Dan Lubbers <lubbers.dan@gmail.com>
2019-10-11 11:32:57 -0400

    kata8

---
+++ Kata-8/kyu8-how-old-will-i-be-in-2099.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/how-old-will-i-be-in-2099/train/javascript
+
+// Philip's just turned four and he wants to know how old he will be in various years in the future such as 2090 or 3044. His parents can't keep up calculating this so they've begged you to help them out by writing a programme that can answer Philip's endless questions.
+
+// Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+
+// Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: "You were born this very year!"
+
+// "..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.
+
+// My Answer
+function  calculateAge(birthYear, futureYear) {
+    let futureAge = futureYear - birthYear;
+    let willBeBorn = birthYear - futureYear;
+
+    if(futureAge === 1) return `You are ${futureAge} year old.` 
+    if(willBeBorn === 1) return `You will be born in ${willBeBorn} year.`
+    if(birthYear === futureYear) return 'You were born this very year!' 
+    if(birthYear < futureYear) return `You are ${futureAge} years old.` 
+    if(birthYear > futureYear) return `You will be born in ${willBeBorn} years.`
+}
+
+// Best Practice
+function calculateAge(m , n) {
+    if(m === n) return 'You were born this very year!';
+    let year = Math.abs(m-n) === 1 ? 'year' : 'years';
+    if(m < n) return "You are " + (n-m) + " " + year + ' old. ';
+    if(m > n) return "You will be born in " + (n-m) + " " + year + '.';
+}
+
+console.log(calculateAge(2012, 2016),"You are 4 years old.");
+console.log(calculateAge(1989, 2016),"You are 27 years old.");
+console.log(calculateAge(2000, 2090),"You are 90 years old.");
+console.log(calculateAge(2000, 1990),"You will be born in 10 years.");
+console.log(calculateAge(3400, 3400),"You were born this very year!"); 
+console.log(calculateAge(900, 2900),"You are 2000 years old.");
+console.log(calculateAge(2010, 1990),"You will be born in 20 years.");
+console.log(calculateAge(2010, 1500),"You will be born in 510 years.");
+console.log(calculateAge(2011, 2012),"You are 1 year old.");
+console.log(calculateAge(2000, 1999),"You will be born in 1 year.");
