369b3d5b73344369f69cd1a96b9e2ff5196a4375
Dan Lubbers <lubbers.dan@gmail.com>
2019-11-22 12:08:42 -0500

    kata7

---
+++ Kata-7/kyu7-find-the-longest-gap.js
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/find-the-longest-gap/train/javascript
+
+// A binary gap within a positive number num is any sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of num.
+// For example:
+// 9 has binary representation 1001 and contains a binary gap of length 2.
+// 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3.
+// 20 has binary representation 10100 and contains one binary gap of length 1.
+// 15 has binary representation 1111 and has 0 binary gaps.
+// Write function gap(num) that, given a positive num, returns the length of its longest binary gap.
+// The function should return 0 if num doesn't contain a binary gap.
+
+// My Answer
+function gap(num) {
+  const binaries = [...num.toString(2)];
+  let longestGap = 0,
+      currentGap = 0;
+
+  for (const bin of binaries) {
+    if (+bin === 0) currentGap++;
+    if (+bin === 1) {
+      if (currentGap > longestGap)
+      longestGap = currentGap;
+      currentGap = 0;
+    }
+  }
+  return longestGap;
+}
+
+// Most Clever
+function gap(num) {
+  return (num.toString(2).match(/10+(?=1)/g) || [' ']).sort().pop().length -1;
+}
+
+console.log(gap(9),2);
+console.log(gap(529),4);
+console.log(gap(20),1);
+console.log(gap(15),0);
