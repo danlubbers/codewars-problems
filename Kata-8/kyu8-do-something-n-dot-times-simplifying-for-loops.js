00218879325a5ee113e86c998cfd6140115f372c
Dan Lubbers <lubbers.dan@gmail.com>
2019-11-29 16:27:48 -0500

    kata8

---
+++ Kata-8/kyu8-do-something-n-dot-times-simplifying-for-loops.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/do-something-n-dot-times-simplifying-for-loops/train/javascript
+
+// Task Summary
+// In case you got lost, here's precisely what you have to do: define a method Number.prototype.times that accepts a function f as an argument and executes it as many times as the integer it is called on (e.g. (100).times would execute something 100 times). The iteration variable i should be supplied to the anonymous function being executed in order to support looping through array elements.
+
+// My Answer
+Number.prototype.times = function (f) {
+  for(let i = 0 ; i < this; i++) f(i);
+};
+
