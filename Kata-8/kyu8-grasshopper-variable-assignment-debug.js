a3a0a987602930b04765dfca1314246d20c4daf2
Dan Lubbers <lubbers.dan@gmail.com>
2020-01-21 11:07:32 -0500

    kata8

---
+++ Kata-8/kyu8-grasshopper-variable-assignment-debug.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5612e743cab69fec6d000077/train/javascript
+
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+var a = "dev";
+var b = "Lab";
+
+var name = a + b;
+
+
+console.log(a, 'dev')
+console.log(b, 'Lab')
+console.log(name, 'devLab')
