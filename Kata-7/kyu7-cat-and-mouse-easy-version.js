17c50a544369f672299df2a7c637286e22a295bc
Dan Lubbers <lubbers.dan@gmail.com>
2020-02-05 13:13:50 -0500

    kata7

---
+++ Kata-7/kyu7-cat-and-mouse-easy-version.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/57ee24e17b45eff6d6000164/train/javascript
+
+// You will be given a string (x) featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'.
+
+// You need to find out if the cat can catch the mouse from it's current position. The cat can jump over three characters. So:
+
+// C.....m returns 'Escaped!' <-- more than three characters between
+
+// C...m returns 'Caught!' <-- as there are three characters between the two, the cat can jump.
+
+// My Answer
+function catMouse(x) {
+  const caught = x.substring(0, 5);
+  return caught.match(/m/gi) ? 'Caught!' : 'Escaped!';
+}
+
+// Best Practice
+function catMouse(x) {
+  return x.length <= 5 ? 'Caught!' : 'Escaped!';
+}
+
+// Different Regex
+function catMouse(x) {
+  return /C.{0,3}m/.test(x) ? 'Caught!' : 'Escaped!';
+}
+
+console.log(catMouse('C....m'), "Escaped!");
+console.log(catMouse('C..m'), "Caught!");
+console.log(catMouse('C.....m'), "Escaped!");
