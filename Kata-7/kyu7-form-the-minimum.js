6d6d53f34a7d1e8fedf1bc754724ac5bdf301606
Dan Lubbers <lubbers.dan@gmail.com>
2020-02-25 13:16:13 -0500

    kata7

---
+++ Kata-7/kyu7-form-the-minimum.js
@@ -0,0 +1,35 @@
+// https://www.codewars.com/kata/5ac6932b2f317b96980000ca/train/javascript
+
+// Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+// Input >> Output Examples
+// minValue ({1, 3, 1})  ==> return (13)
+// Explanation:
+// (13) is the minimum number could be formed from {1, 3, 1} , Without duplications
+
+// minValue({5, 7, 5, 9, 7})  ==> return (579)
+// Explanation:
+// (579) is the minimum number could be formed from {5, 7, 5, 9, 7} , Without duplications
+
+// minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
+// Explanation:
+// (134679) is the minimum number could be formed from {1, 9, 3, 1, 7, 4, 6, 6, 7} , Without duplications
+
+// My Answer
+function minValue(values){
+  return +[...new Set(values.sort((a, b) => a - b))].join('');
+}
+
+// Getting rid of duplicates using Regex
+function minValue(values) {
+  return values.sort((a, b) => a - b).join('').replace(/(\d)\1*/g, '$1');
+}
+
+console.log(minValue([1, 3, 1]), 13);
+console.log(minValue([4, 7, 5, 7]), 457);
+console.log(minValue([4, 8, 1, 4]), 148);
+console.log(minValue([5, 7, 9, 5, 7]), 579);
+console.log(minValue([6, 7, 8, 7, 6, 6]), 678);
+console.log(minValue([5, 6, 9, 9, 7, 6, 4]), 45679);
+console.log(minValue([1, 9, 1, 3, 7, 4, 6, 6, 7]), 134679);
+console.log(minValue([3, 6, 5, 5, 9, 8, 7, 6, 3, 5, 9]), 356789);
+console.log(minValue([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), 1);
